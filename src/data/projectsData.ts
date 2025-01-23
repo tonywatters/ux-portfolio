79bd3cb47dd2e67e9997e15f1df68eae5dafac68
tony watters <tony.watters@learningpool.com>
2025-01-23 16:54:56 +0000

    getImagePath update 3

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -42,19 +42,19 @@ const getImagePath = (path: string) => {
       title: "Skills Accelerator",
       description: "Users of the Vision website have voiced concerns about the degrading legibility of Vision articles",
       tags: ["UX Research", "User Testing", "Wireframing", "Prototyping"],
-      image: getImagePath("/ux-portfolio/images/cards/portfolio-1.jpg"),
-      mainImage: getImagePath("/ux-portfolio/images/pages/ArticleImage_2.avif"),
+      image: getImagePath("/images/cards/portfolio-1.jpg"),
+      mainImage: getImagePath("/images/pages/ArticleImage_2.avif"),
       challenge: "The company needed to upskill a workforce of nearly 60,000 employees to prepare for future work demands and internal talent development.",
       process: "We developed a SharePoint website with custom web parts, enabling employees to: View current skillsets. Identify opportunities to upskill and expand their knowledge. This solution also facilitated internal recruitment by leveraging existing skillsets and fostering in-house talent development.",
       solution: "We created interactive features to support employee upskilling. The Get Started Card helped employees build their Eightfold profiles, while Call to Action Cards provided access to resources like role exploration, skills learning, and career coaching. Skills Cards shared company announcements, the Jobs Card highlighted internal roles with application options, and a Career Advice Booking System enabled easy scheduling with career coaches.",
       results: "Prototypes and Designs Logo designs for branding various features. Prototypes built in Adobe XD to visualize user interactions and workflows.",
       processImages: [
-        getImagePath("/ux-portfolio/images/pages/skills_1.jpg"),
-        getImagePath("/ux-portfolio/images/pages/skills_2.jpg")
+        getImagePath("/images/pages/skills_1.jpg"),
+        getImagePath("/images/pages/skills_2.jpg")
       ],
       resultImages: [
-        getImagePath("/ux-portfolio/images/pages/skills_3.jpg"),
-        getImagePath("/ux-portfolio/images/pages/skills_4.jpg")
+        getImagePath("/images/pages/skills_3.jpg"),
+        getImagePath("/images/pages/skills_4.jpg")
       ]
     },
     {
@@ -62,15 +62,15 @@ const getImagePath = (path: string) => {
       title: "LCMS Key Metrics feature",
       description: "The Key Metrics feature in the Content Details tab provides administrators with an overview of content performance, showing metrics like Total Learners, Completion Rate, and Success Rate. With filtering options for specific Accounts or Licenses, it enables targeted analysis and supports data-driven decisions to improve content effectiveness and learner engagement.",
       tags: ["Information Architecture", "Data Visualization", "Wirefames", "Prototyping"],
-      image: getImagePath("/ux-portfolio/images/cards/keymetrics1.png"),
-      mainImage: getImagePath("/ux-portfolio/images/pages/keymetrics1.png"),
+      image: getImagePath("/images/cards/keymetrics1.png"),
+      mainImage: getImagePath("/images/pages/keymetrics1.png"),
       challenge: "The feature includes a filtering mechanism allowing admins to view metrics across all data or drill down into specific Accounts or Licenses, enabling quick assessment of content effectiveness across different user segments. This addition to the Content Details streamlines the process of evaluating content performance, supporting data-driven decision-making for content improvement and learner engagement strategies.",
       process: "I analyzed workflows, created wireframes, designed high-fidelity prototypes, ensured responsiveness and accessibility, and collaborated with developers to deliver a seamless implementation.",
       solution: "We delivered low-fidelity wireframes, high-fidelity mockups, interactive prototypes, and detailed design specifications to streamline the design process. The solution focused on creating an intuitive, filterable Key Metrics feature for the Content Details tab, providing administrators with improved workflows and accessible interfaces.",
       results: "The design enhanced admin workflows for content performance analysis, improved data visualization, and enabled intuitive filtering for segmented data views. The interface adhered to WCAG guidelines, received positive stakeholder feedback, and ensured a smooth handoff to the development team, driving successful implementation.",
       processImages: [
-        getImagePath("/ux-portfolio/images/pages/keymetrics2.png"),
-        getImagePath("/ux-portfolio/images/pages/keymetrics3.png")
+        getImagePath("/images/pages/keymetrics2.png"),
+        getImagePath("/images/pages/keymetrics3.png")
       ]
     }
  ];
