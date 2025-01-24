d4f91cda1e726f5bd815d10254652352a40ee3d7
tony watters <tony.watters@learningpool.com>
2025-01-24 11:05:23 +0000

    getImagePath updates

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,15 @@
 export const scrollToSection = (sectionId: string, currentPath: string) => {
-    if (currentPath.includes('/project/')) {
-      window.location.href = `/ux-portfolio/#${sectionId}`;
-    } else {
-      const element = document.getElementById(sectionId);
-      element?.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
+  if (currentPath.includes('/project/')) {
+    window.location.href = `/ux-portfolio/#${sectionId}`;
+  } else {
+    const element = document.getElementById(sectionId);
+    element?.scrollIntoView({ behavior: 'smooth' });
+  }
+};
+
+export const getImagePath = (path: string) => {
+  const cleanPath = path.replace(/^\/ux-portfolio\//, '');
+  return process.env.NODE_ENV === 'production'
+    ? `/ux-portfolio/${cleanPath}`
+    : `/${cleanPath}`;
+};
